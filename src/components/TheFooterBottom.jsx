265ec0d24e703bc52f9d9ef074519b699cb02401
YosefCpo <yosefheshambusiness@gmail.com>
2025-07-04 02:29:14 +0300

    fdgdfgdffg

--- src/components/TheFooterBottom.jsx
+++ src/components/TheFooterBottom.jsx
@@ -6,14 +6,14 @@ function TheFooterBottom() {
         <div className="bg-[#1a1c37] text-white">
             <div className="line w-full h-[2px] bg-white"></div>
             <div className="container py-[30px] flex items-center justify-between gap-[30px] max-md:flex-col">
-                <div className="info flex flex-col gap-[25px]">
+                <div className="info flex flex-col gap-[25px] max-md:order-1">
                     <p>Egypt &copy; 2015-2025 Pescado Pharmaceuticals. All rights reserved.</p>
-                    <p className="flex items-center">
+                    <p className="flex items-center max-md:justify-center">
                         Powered By <img className="max-w-full w-[250px]" src={BlueDashLogo} />
                     </p>
                 </div>
-                <div className="image">
-                    <img className="w-[300px]" src={PescadoLogo} />
+                <div className="image max-md:order-[0]">
+                    <img className="w-[200px]" src={PescadoLogo} />
                 </div>
             </div>
         </div>
